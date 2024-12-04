b4eea8a05ce4bbd22c97711997a2d3fe342e684c
Mauzerov <mauzer.official.1@gmail.com>
2024-12-04 17:33:20 +0100

    fix stork not killing player when player moved into it

--- stork.h
+++ stork.h
@@ -20,14 +20,13 @@ void update_stork(struct Game * game) {
     struct Point delta = { 0 },
                * stork_pos = &game->stork->pos;
     struct Entity * stork = game->stork;
-    if (!can_move(stork))
-        return;
-    delta.x = game->player.x - stork_pos->x;
-    delta.y = game->player.y - stork_pos->y;
+    if (can_move(stork)) {
+        delta.x = game->player.x - stork_pos->x;
+        delta.y = game->player.y - stork_pos->y;
 
-    stork_pos->y += sgn(delta.y);
-    stork_pos->x += sgn(delta.x);
-
+        stork_pos->y += sgn(delta.y);
+        stork_pos->x += sgn(delta.x);
+    }
     if (point_overlapping(stork_pos, &game->player)) {
         game->over = LOSS;
     }
